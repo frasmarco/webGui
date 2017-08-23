5372470549ae4ef7a3c48ee69b11e1ec8006c1fe
Frassinelli Marco <fras.marco@gmail.com>
2017-08-24 00:01:31 +0200

    map: add controls

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -3,18 +3,32 @@ import Map from 'ol/map';
 import View from 'ol/view';
 import TileLayer from 'ol/layer/tile';
 import XYZ from 'ol/source/xyz';
+import Control from 'ol/control';
+import FullScreen from 'ol/control/fullscreen';
+import MousePosition from 'ol/control/mouseposition';
+import ScaleLine from 'ol/control/scaleline';
+import Interaction from 'ol/interaction';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    const map = new Map({
+    const controls: ol.Collection<ol.control.Control> = Control.defaults() ;
+    controls.extend([new FullScreen,
+      new MousePosition,
+      new ScaleLine]);
+
+    const map: Map = new Map({
+      controls: controls,
+//      interactions: Map.interaction.defaults().extend([
+//        new Interaction.DragRotateAndZoom()
+//      ]),
       target: 'map',
       layers: [
         new TileLayer({
