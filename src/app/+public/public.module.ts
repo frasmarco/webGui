ecd04a856b63c9439b6caeecdb22d638d6cfe610
Frassinelli Marco <fras.marco@gmail.com>
2017-08-23 22:01:56 +0200

    Map:  very first implementation

--- src/app/+public/public.module.ts
+++ src/app/+public/public.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PublicRouting } from './public.routing';
 import { SmartadminModule } from '../shared/smartadmin.module';
 import { PublicComponent } from './public.component';
+import { MapModule } from '../shared/map/map.module';
 
 @NgModule({
-  imports: [CommonModule, PublicRouting, SmartadminModule],
+  imports: [CommonModule, PublicRouting, SmartadminModule, MapModule],
   declarations: [PublicComponent]
 })
 export class PublicModule {}
---
+++ src/app/shared/map/map.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/map';
+import View from 'ol/view';
+import TileLayer from 'ol/layer/tile';
+import XYZ from 'ol/source/xyz';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+
+}
